4252d67ca0884d5bbe18e3869153b780e8d36473
Liannthu <76727373+Liannthu@users.noreply.github.com>
2021-04-07 15:44:05 +0800

    Add files via upload

---
+++ sobel_tlm_bus_/filter_def.h
@@ -0,0 +1,21 @@
+#ifndef FILTER_DEF_H_
+#define FILTER_DEF_H_
+
+// sobel mask parameters
+const int MASK_N = 2;
+const int MASK_X = 3;
+const int MASK_Y = 3;
+
+// Sobel Filter inner transport addresses
+// Used between blocking_transport() & do_filter()
+const int SOBEL_FILTER_R_ADDR = 0x00000000;
+const int SOBEL_FILTER_RESULT_ADDR = 0x00000004;
+
+union word {
+  int sint;
+  unsigned int uint;
+  unsigned char uc[4];
+};
+
+// Sobel mask
+#endif
